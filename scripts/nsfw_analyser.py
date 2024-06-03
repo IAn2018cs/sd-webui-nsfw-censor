95c140b64413451aba4276080441826f923a4500
chenshuai <chenshuai@amberweather.com>
2024-06-03 16:28:40 +0800

    add default img

--- scripts/nsfw_analyser.py
+++ scripts/nsfw_analyser.py
@@ -8,8 +8,19 @@ from nsfw.core import run
 
 
 def image_analyser(
+	replacement_img: Image.Image,
 	target_img: Image.Image
 ) -> Image.Image:
+	if isinstance(replacement_img, str):  # source_img is a base64 string
+		import base64, io
+		if 'base64,' in replacement_img:  # check if the base64 string has a data URL scheme
+			base64_data = replacement_img.split('base64,')[-1]
+			img_bytes = base64.b64decode(base64_data)
+		else:
+			# if no data URL scheme, just decode
+			img_bytes = base64.b64decode(replacement_img)
+		replacement_img = Image.open(io.BytesIO(img_bytes))
+
 	target_path = tempfile.NamedTemporaryFile(delete=False, suffix=".png").name
 	target_img.save(target_path)
 
@@ -17,7 +28,7 @@ def image_analyser(
 	if result is None:
 		result_image = target_img
 	elif result:
-		result_image = None
+		result_image = replacement_img
 	else:
 		result_image = target_img
 
--- scripts/nsfw_censor_script.py
+++ scripts/nsfw_censor_script.py
@@ -7,8 +7,8 @@ from modules.processing import (
 	StableDiffusionProcessing,
 )
 
-from scripts.nsfw_utils import get_timestamp
 from scripts.nsfw_analyser import image_analyser
+from scripts.nsfw_utils import get_timestamp
 
 sc_name = "NSFW-CENSOR"
 version = "1.0"
@@ -31,17 +31,25 @@ class NsfwCensorScript(scripts.Script):
 				with gr.Row():
 					gr.Markdown(value=f"v{version}")
 				with gr.Row():
+					img = gr.Image(type="pil", label="nsfw picture replacement picture")
+				with gr.Row():
 					enable = gr.Checkbox(False, placeholder="enable", label="Enable")
 		return [
+			img,
 			enable
 		]
 
 	def process(
 		self,
 		p: StableDiffusionProcessing,
+		img,
 		enable
 	):
+		self.replacement_img = img
 		self.enable = enable
+		if self.enable:
+			if self.replacement_img is None:
+				print(f"Please provide a replacement img")
 
 	def postprocess_batch(self, *args, **kwargs):
 		if self.enable:
@@ -49,17 +57,19 @@ class NsfwCensorScript(scripts.Script):
 
 	def postprocess_image(self, p, script_pp: scripts.PostprocessImageArgs, *args):
 		if self.enable:
-			st = get_timestamp()
-			print(f"{sc_name} enabled, start process")
-			image: Image.Image = script_pp.image
+			if self.replacement_img is not None:
+				st = get_timestamp()
+				print(f"{sc_name} enabled, start process")
+				image: Image.Image = script_pp.image
 
-			result: Image.Image = image_analyser(
-				image
-			)
-			pp = scripts_postprocessing.PostprocessedImage(result)
-			pp.info = {}
-			p.extra_generation_params.update(pp.info)
-			script_pp.image = pp.image
-			et = get_timestamp()
-			cost_time = (et - st) / 1000
-			print(f"{sc_name} process done, time taken: {cost_time} sec.")
+				result: Image.Image = image_analyser(
+					self.replacement_img,
+					image
+				)
+				pp = scripts_postprocessing.PostprocessedImage(result)
+				pp.info = {}
+				p.extra_generation_params.update(pp.info)
+				script_pp.image = pp.image
+				et = get_timestamp()
+				cost_time = (et - st) / 1000
+				print(f"{sc_name} process done, time taken: {cost_time} sec.")
